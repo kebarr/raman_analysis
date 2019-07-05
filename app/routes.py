2e81127dbcfe65f5992ea762e13ad9d48cbb20cc
Katie Barr <katarzinia@gmail.com>
2019-07-05 12:48:16 +0100

    figured out request string format

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
 from app import app
 
+#@app.route('/')
+#@app.route('/index')
+#def index():
+#    return "Hello, World!"
+
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World!"
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/initialise_matcher/<material_name>/<data_filename>/<int:subtract_baseline>')
+def initialise_matcher(material_name, data_filename, subtract_baseline):
+    return "material_name: %s, data_filename: %s, subtract_baseline: %d " % (material_name, data_filename, subtract_baseline)
+
+
+
