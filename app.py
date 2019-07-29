exposing more info to front end, confused about peak calculation bug

app.py
@@ -142,15 +142,16 @@ def get_example_matches(fm, confidence="medium", number_to_plot=2):
     print(confidence)
     matches = fm.get_condifence_matches(confidence)
     number_matches = len(matches)
-    print(len(matches[0]), len(matches))
     index_to_plot_1 = np.random.randint(0, number_matches)
     index_to_plot_2 = np.random.randint(0, number_matches)
-    m1 = matches[index_to_plot_1][2]
-    m2 = matches[index_to_plot_2][2]
-    cp1 = matches[index_to_plot_1][3]
-    cp2 = matches[index_to_plot_2][3]
-    c1 = matches[index_to_plot_1][1]
-    c2 = matches[index_to_plot_2][1]
+    match1 = matches[index_to_plot_1]
+    match2 = matches[index_to_plot_2]
+    m1 = match1.spectrum
+    m2 = match2.spectrum
+    cp1 = match1.conv_peaks
+    cp2 = match2.conv_peaks
+    c1 = match1.confidence
+    c2 = match2.confidence
     print("confidence 1: %d, confidence 2: %d" % (c1, c2) )
     print(cp1)
     print(cp2)
@@ -167,12 +168,13 @@ def get_example_matches(fm, confidence="medium", number_to_plot=2):
     io = StringIO()
     fig.savefig(io, format='png')
     plt.close(fig)
-    return number_matches, base64.encodestring(io.getvalue())
+    return number_matches, base64.encodestring(io.getvalue()), match1, match2
 
 def plot_example_match(fm):
-    number_matches_med, data = get_example_matches(fm, "medium", number_to_plot=2)
+    number_matches_med, data, match1, match2 = get_example_matches(fm, "medium", number_to_plot=2)
     number_matches_high = len(fm.matches.high_confidence)
     all_matches = len(fm.matches.matches)
+    print(match1.peak_data, match2.peak_data)
     template_data = fm.material.template
     fig, (ax1) = plt.subplots(1,1, sharex=True, sharey=True, figsize=(13, 5))
     ax1.set(xlabel = 'Shift (cm$^{-1}$)')
@@ -184,7 +186,7 @@ def plot_example_match(fm):
     template = base64.encodestring(io.getvalue())
     if number_matches_med == 0:
         return render_template('plot_data.html', number_matches_med=number_matches_med, number_matches_high=number_matches_high, all_matches= all_matches, template = template,  number_locations=fm.len, filename=fm.data_filename, material="graphene_oxide", subtract_baseline=False) 
-    return render_template('plot_data.html', number_matches_med=number_matches_med, number_matches_high=number_matches_high, all_matches= all_matches, template = template,  number_locations=fm.len, match_example=data, filename=fm.data_filename, material="graphene_oxide", subtract_baseline=False)
+    return render_template('plot_data.html', number_matches_med=number_matches_med, number_matches_high=number_matches_high, all_matches= all_matches, template = template,  number_locations=fm.len, match_example=data, filename=fm.data_filename, material="graphene_oxide", subtract_baseline=False, match1 = match1.to_dict(), match2=match2.to_dict())
 
 @app.route('/uploadajax', methods = ['POST'])
 def upload_image():
@@ -213,8 +215,7 @@ def upload_image():
                 return {'image': img_str, 'output_filename': output_filename}
 
 
-# (u'uploads/D_M1_Spleen_Slide3_2019_02_11_14_43_47.txt', u'graphene_oxide', False)
-# (u'uploads/D_M1_Spleen_Slide3_2019_02_11_14_43_47.txt', u'graphene_oxide', False)
+
 # now need to wire this in to plot example baseline
 @app.route('/plot_med', methods = ['POST'])
 def plot_med():
@@ -225,10 +226,13 @@ def plot_med():
     print(data_filename, material, sb)
     fm = find_material(data_filename, material, sb_bool)
     print("found material")
-    _, img_str = get_example_matches(fm, confidence="medium", number_to_plot=2)
+    _, img_str, match1, match2 = get_example_matches(fm, confidence="medium", number_to_plot=2)
     print("got example matches")
     # actually_do_the_stuff is called.... whyu!?!?!?!?!?
-    return {'image': img_str}
+    return {'image': img_str, 'match1_confidence': match1.confidence, 'match2_confidence': match2.confidence, \
+        'd_intensity1':match1.peaks[0][1], 'g_intensity1': match1.peaks[1][1], 'peak_ratio1': match1.peak_ratio, \
+            'd_intensity2':match2.peaks[0][1], 'g_intensity2': match2.peaks[1][1], 'peak_ratio2': match2.peak_ratio,\
+                'x1': match1.x, 'y1': match1.y, 'x2': match2.x, 'y2': match2.y}
 
 @app.route('/plot_high', methods = ['POST'])
 def plot_high():
@@ -238,10 +242,12 @@ def plot_high():
     sb_bool = True if sb == "True" else False
     fm = find_material(data_filename, material, sb_bool)
     print("found material")
-    _, img_str = get_example_matches(fm, confidence="high", number_to_plot=2)
+    _, img_str, match1, match2 = get_example_matches(fm, confidence="high", number_to_plot=2)
     print("got example matches")
-    return {'image': img_str}
-
+    return {'image': img_str, 'match1_confidence': match1.confidence, 'match2_confidence': match2.confidence, \
+        'd_intensity1':match1.peak_data[0][1], 'g_intensity1': match1.peak_data[1][1], 'peak_ratio1': match1.peak_ratio, \
+            'd_intensity2':match2.peak_data[0][1], 'g_intensity2': match2.peak_data[1][1], 'peak_ratio2': match2.peak_ratio,\
+                'x1': match1.x, 'y1': match1.y, 'x2': match2.x, 'y2': match2.y}
 
 @app.route('/download_image', methods=['GET', 'POST'])
 def download_image():
@@ -281,31 +287,7 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-# grep -rno 'jquery-file-upload.appspot.com' .
-#./index.html:15:jquery-file-upload.appspot.com
-#./templates/index.html:15:jquery-file-upload.appspot.com
-#./flask-file-uploader/static/js/main.js:37:jquery-file-upload.appspot.com
-#./flask-file-uploader/static/js/main.js:49:jquery-file-upload.appspot.com
-#./flask-file-uploader/static/js/app.js:19:jquery-file-upload.appspot.com
-#./flask-file-uploader/templates/index.html:15:jquery-file-upload.appspot.com
-
-#sed -i -e 's,jquery-file-upload.appspot.com,flask-file-uploader/static/js/jQuery-File-Upload-9.32.0/,g' ./index.html
-#sed -i -e 's,jquery-file-upload.appspot.com,flask-file-uploader/static/js/jQuery-File-Upload-9.32.0/,g' ./templates/index.html
-#sed -i -e 's,jquery-file-upload.appspot.com,flask-file-uploader/static/js/jQuery-File-Upload-9.32.0/,g' ./flask-file-uploader/static/js/main.js
-#sed -i -e 's,jquery-file-upload.appspot.com,flask-file-uploader/static/js/jQuery-File-Upload-9.32.0/,g' ./flask-file-uploader/static/js/app.js
-#sed -i -e 's,jquery-file-upload.appspot.com,flask-file-uploader/static/js/jQuery-File-Upload-9.32.0/,g' ./flask-file-uploader/templates/index.html
-#sed -i -e 's,http://127.0.0.1:5000/flask-file-uploader,flask-file-uploader,g' ./flask-file-uploader/static/js/main.js
-#sed -i -e 's,http://127.0.0.1:5000/flask-file-uploader,flask-file-uploader,g' ./flask-file-uploader/static/js/app.js
-#sed -i -e 's,http://127.0.0.1:5000/flask-file-uploader,flask-file-uploader,g' ./index.html
-#sed -i -e 's,http://127.0.0.1:5000/flask-file-uploader,flask-file-uploader,g'./templates/index.html
-#sed -i -e 's,http://127.0.0.1:5000/flask-file-uploader,flask-file-uploader,g' ./flask-file-uploader/templates/index.html
-
-#sed -i -e 's,127.0.0.1:5000/flask-file-uploader,jquery-file-upload.appspot.com,g' ./flask-file-uploader/static/js/main.js
-#sed -i -e 's,127.0.0.1:5000/flask-file-uploader,jquery-file-upload.appspot.com,g' 
-# (u'uploads/D_M1_Spleen_Slide3_2019_02_11_14_43_47.txt', 'graphene_oxide', False)
-
-# first call: (u'uploads/D_M1_Spleen_Slide3_2019_02_11_14_43_47.txt', 'graphene_oxide', u'False')
-# is now: (u'uploads/D_M1_Spleen_Slide3_2019_02_11_14_43_47.txt', u'graphene_oxide', u'False')
-#         (u'uploads/D_M1_Spleen_Slide3_2019_02_11_14_43_47.txt', u'graphene_oxide', u'False')
-# theyt're exactly the same so pwhat is the issue!!!
-# id/ig ratio
+# minimum to do!!
+# id/ig ratio - find max in g peak region and max in d peak region, not robust to cosmic rays but my baselining removes these.
+# show for shown match, confidence score, d/g ratio
+# user output - image of spectrum, of currently shown match (so need to separate somehow, as its bytecode!!) 

convolution_matching.py
@@ -100,10 +100,10 @@ class FindMaterial(object):
 
     def find_indices_of_peak_wavelengths(self):
         ##TODO - THIS ASSUMES TWO PEAKS!!! - just make a list and append pairs
-        lower_bound_1 = self.material.peaks[0][0]
-        upper_bound_1 = self.material.peaks[0][1]
-        lower_bound_2 = self.material.peaks[1][0]
-        upper_bound_2 = self.material.peaks[1][1]
+        self.lower_bound_1 = self.material.peaks[0][0]
+        self.upper_bound_1 = self.material.peaks[0][1]
+        self.lower_bound_2 = self.material.peaks[1][0]
+        self.upper_bound_2 = self.material.peaks[1][1]
         #print(self.wavelengths)
         cond = ((self.wavelengths > lower_bound_1) & (self.wavelengths < upper_bound_1)) | ((self.wavelengths > lower_bound_2) & (self.wavelengths < upper_bound_2))
         self.peak_indices = np.where(cond)
@@ -141,28 +141,31 @@ class FindMaterial(object):
     def check_match(self, data, index):
         peak_start = self.peak_indices[0][0]
         peak_end = self.peak_indices[0][-1]
+        spectrum = data.iloc[index]
         # check proposed match by comaring mean of peak region to mean of non peak region
         # this assumes peaks are close enough together to be treated as one block
-        max_peaks = np.max(data.iloc[index][peak_start:peak_end]) + 50
+        max_peaks = np.max(spectrum[peak_start:peak_end]) + 50
         # cut off first bit cos there's some weirdness in Cyrills data.
-        mean_non_peaks = (np.mean(data.iloc[index][200:self.peak_indices[0][0]]) + np.mean(data.iloc[index][self.peak_indices[0][-1]:]))*0.5 + 50
-        stdev_non_peaks = np.std(np.concatenate([data.iloc[index][200:self.peak_indices[0][0]], data.iloc[index][self.peak_indices[0][-1]:]]))
+        mean_non_peaks = (np.mean(spectrum[200:self.peak_indices[0][0]]) + np.mean(spectrum[self.peak_indices[0][-1]:]))*0.5 + 50
+        stdev_non_peaks = np.std(np.concatenate([spectrum[200:self.peak_indices[0][0]], spectrum[self.peak_indices[0][-1]:]]))
         # TODO- confidence scores can be high when mean of data is close to 0, even for pretty shitty matches, 
         # try basing off standard deviation near peaks
         #print("mean peaks: ", mean_peaks, " mean non peaks: ", mean_non_peaks, " stdev non peaks ", stdev_non_peaks)
         if max_peaks > mean_non_peaks+5*stdev_non_peaks:# be quite forgiving as cosmic rays etc will mess it up
+            print(spectrum.shape)
+            peak_data = self.get_peak_heights(mean_non_peaks, stdev_non_peaks, spectrum)
             # calculate how far beyond non peak mean as a confidence measure
             if max_peaks < (2000+mean_non_peaks):
                 confidence = (100.*max_peaks)/(2000+mean_non_peaks)
                 print("mean peaks %d mean non peaks %d confidence %d " % (max_peaks, mean_non_peaks, confidence))
             else:
                 confidence = 100
-            return True, confidence
+            return True, confidence, peak_data
         else:
-            return False, 0
+            return False, 0, [0]
 
     def is_match(self, data, index):
-        res, con = self.check_match(data, index)
+        res, con, peak_data = self.check_match(data, index)
         if res:
             to_match = self.prepare_data(data, index)
             template = self.material.template
@@ -171,8 +174,8 @@ class FindMaterial(object):
             if len(conv_peaks[0]) == 0:
                 return False, 0, [0]
             elif len(conv_peaks[0]) > 0:
-                return True, con, conv_peaks
-        return False, 0, [0]
+                return True, con, conv_peaks, peak_data
+        return False, 0, [0], [0]
 
     def find_matches(self, data):
         self.find_indices_of_peak_wavelengths()
@@ -182,11 +185,21 @@ class FindMaterial(object):
         for i in range(number_locations):
             if i%update_flag == 0:
                 print("Tested %d locations, found %d matches" % (i, len(self.matches.matches)))
-            match, con, conv_peaks = self.is_match(data, i)
+            match, con, conv_peaks, peak_data = self.is_match(data, i)
             if match == True:
                 print(i, " con: ", con, "max: ",  np.max(data.iloc[i]), " conv_peaks ", conv_peaks)
-                self.matches.add_match(i, con, data.iloc[i], conv_peaks)
+                match_position = self.get_match_position(i)
+                self.matches.add_match(self.material, con, data.iloc[i], conv_peaks, peak_data, match_position)
         print("Finished finding matches, found %d locations positive for %s" % (len(self.matches.matches), self.material_name))
+
+    def get_match_position(self, i):
+        x = int(i)/int(self.len_y)
+        if x == 0:
+            # avoid division by 0
+            y = i
+        else:
+            y = i%(x*self.len_y)
+        return (x, y)
 
     def get_condifence_matches(self, thresh='medium'):
         print(thresh)
@@ -213,7 +226,31 @@ class FindMaterial(object):
         else:
             matches = self.matches.matches
         # matches is (match_index, confidence score)
-        for match, confidence, _, _ in matches:
-            mi.add_value_to_image(match, confidence)
+        for match in matches:
+            mi.add_value_to_image(match )
         mi.save_image(output_filename)
-        
+
+    def get_peak_heights(self, mean_non_peaks, stdev_non_peaks, spectrum):
+        print("spectrum shape: ", spectrum.shape)
+        results = []
+        peaks = 0
+        for peak in range(len(self.material.peaks)):
+            peak_present, peak_max = self.peak_at_location(peak, mean_non_peaks, stdev_non_peaks, spectrum)
+            results.append((peak_present, peak_max))
+            if peak_present:
+                peaks += 1
+        return results
+
+    def peak_at_location(self, peak_ind, mean_non_peaks, stdev_non_peaks, spectrum):
+        print(spectrum.shape)
+        peak_bounds = self.material.peaks[peak_ind]
+        print(peak_bounds)
+        peak = spectrum[peak_bounds[0]:peak_bounds[1]]
+        print(peak.shape)
+        peak_max = np.max(peak)
+        print("peak max: ", peak_max)
+        peak_present = False
+        if peak_max > mean_non_peaks+5*stdev_non_peaks:
+            peak_present = True
+        return peak_present, peak_max
+

match_support_classes.py
@@ -1,27 +1,52 @@
 import numpy as np
 from PIL import Image
 
+# container for match stuff to improve readability
+class Match(object):
+    def __init__(self, material, confidence, spectrum, conv_peaks, peak_data, location):
+        self.material = material
+        self.confidence = confidence
+        self.spectrum = spectrum
+        self.conv_peaks = conv_peaks
+        self.peak_data = peak_data
+        self.x = location[0]
+        self.y = location[1]
+        self.peak_ratio = self.calculate_peak_ratio()
+
+    def calculate_peak_ratio(self):
+        number_peaks = len(self.material.peaks)
+        peaks_present = 0
+        peak_ratio = 0
+        for peak in self.peak_data:
+            if peak[0]:
+                peaks_present +=1
+        if number_peaks == 2 and peaks_present == number_peaks:
+            # not sure how to do this if more than 2 peaks
+            peak_ratio = np.float(self.peak_data[0][1])/self.peak_data[1][1]
+        return peak_ratio
+
+    def to_dict(self):
+        return {'confidence': self.confidence, 'spectrum': self.spectrum, 'x' : self.x, 'y': self.y, 'peak_ratio':self.peak_ratio, 'peak_data': self.peak_data}
+
 class Matches(object):
-    def __init__(self, filename, material_name, med_thresh=35, high_thresh=65): 
+    def __init__(self, filename, material, med_thresh=35, high_thresh=50): 
         self.filename = filename
-        self.material_name = material_name
+        self.material = material
         self.med_thresh = med_thresh
         self.high_thresh = high_thresh
         self.matches = []
         self.high_confidence = []
         self.med_confidence = []
-        print(self.med_thresh, " med thresh")
-        print(self.high_thresh, " med thresh")
 
-    def add_match(self, index, confidence, spectrum, conv_peaks):
-        self.matches.append((index, confidence, spectrum, conv_peaks))
+    def add_match(self, material, confidence, spectrum, conv_peaks, peak_data, location):
+        self.matches.append(Match(material, confidence, spectrum, conv_peaks, peak_data, location))
         if confidence > self.high_thresh:
-            print("above high thresh: ", confidence, " index ", index, " thresh ", self.high_thresh)
             self.high_confidence.append(len(self.matches) -1)
             self.med_confidence.append(len(self.matches) -1)    
         elif confidence > self.med_thresh:
-            print("above med thresh: ", confidence, " index ", index, " thresh ", self.med_thresh)
             self.med_confidence.append(len(self.matches) -1)
+
+    
         
 
 class MatchImage(object):
@@ -29,7 +54,6 @@ class MatchImage(object):
         self.len_x = x
         self.len_y = y
         print(x, y)
-        #self.match_image = np.zeros((self.len_x, self.len_y, 4), np.uint8)
 
     def add_image(self, image_filename):
         # in Andy's code, it looks like he just resizes to make both the same dimensions!!
@@ -40,13 +64,8 @@ class MatchImage(object):
         self.im_array = np.array(self.im)
         print(self.im_array.shape)
 
-    def add_value_to_image(self, i, con):
-        x = int(i)/int(self.len_y)
-        if x == 0:
-            # avoid division by 0
-            y = i
-        else:
-            y = i%(x*self.len_y)
+    def add_value_to_image(self, match):
+        con = match.confidence
         # scale it manually to increase contrast
         if con > 70:
             contrast = 100
@@ -59,7 +78,7 @@ class MatchImage(object):
         else:
             contrast = 10 
         # just override previous value in image
-        self.im_array[x+2, y+1] = [255, 128, 0, np.uint8(con*0.01*255)]
+        self.im_array[match.x+2, match.y+1] = [255, 128, 0, np.uint8(con*0.01*255)]
 
     # need to ask Andy about this
     def save_image(self, output_filename):