2a42974bbc6773cb0d7ed1dc6a52e4c1c53214ed
kebarr <katarzinia@gmail.com>
2019-08-01 14:51:34 +0100

    green instead of orange

--- match_support_classes.py
+++ match_support_classes.py
@@ -80,26 +80,26 @@ class MatchImage(object):
         if con > 70:
             contrast = 100
         elif con > 60:
-            contrast = 80
+            contrast = 30
         elif con > 45:
-            contrast = 65 
+            contrast = 20 
         elif con > 30:
-            contrast = 40
+            contrast = 10
         else:
-            contrast = 10 
+            contrast = 5 
         # just override previous value in image
-        self.im_array[int(self.x_scale_factor*(match.x- self.x_0)), int(self.y_scale_factor*(match.y-self.y_0))] = [255, 128, 0, np.uint8(con*0.01*255)]
-        x_to_add = int(self.x_scale_factor)*2
-        y_to_add = int(self.y_scale_factor)*2
+        self.im_array[int(self.x_scale_factor*(match.x- self.x_0)), int(self.y_scale_factor*(match.y-self.y_0))] = [57, 255, 20, np.uint8(con*0.01*255)]
+        x_to_add = int(self.x_scale_factor+2)
+        y_to_add = int(self.y_scale_factor+2)
         if int(self.x_scale_factor*(match.x- self.x_0)) + x_to_add -1 < len(self.im_array):
             if int(self.y_scale_factor*(match.y- self.y_0)) + y_to_add -1 < len(self.im_array[0]):
                 for i in range(x_to_add):
                     for j in range(y_to_add):
                         # as scaled up, need to colous other pixels according to scale factor
-                        self.im_array[int(self.x_scale_factor*(match.x- self.x_0))+i, int(self.y_scale_factor*(match.y-self.y_0))+j] = [255, 128, 0, np.uint8(con*0.01*255)]
-                        self.im_array[int(self.x_scale_factor*(match.x- self.x_0))-i, int(self.y_scale_factor*(match.y-self.y_0))-j] = [255, 128, 0, np.uint8(con*0.01*255)]
-                        self.im_array[int(self.x_scale_factor*(match.x- self.x_0))+i, int(self.y_scale_factor*(match.y-self.y_0))-j] = [255, 128, 0, np.uint8(con*0.01*255)]
-                        self.im_array[int(self.x_scale_factor*(match.x- self.x_0))-i, int(self.y_scale_factor*(match.y-self.y_0))+j] = [255, 128, 0, np.uint8(con*0.01*255)]
+                        self.im_array[int(self.x_scale_factor*(match.x- self.x_0))+i, int(self.y_scale_factor*(match.y-self.y_0))+j] = [57, 255, 20, np.uint8(con*0.01*255)]
+                        self.im_array[int(self.x_scale_factor*(match.x- self.x_0))-i, int(self.y_scale_factor*(match.y-self.y_0))-j] = [57, 255, 20, np.uint8(con*0.01*255)]
+                        self.im_array[int(self.x_scale_factor*(match.x- self.x_0))+i, int(self.y_scale_factor*(match.y-self.y_0))-j] = [57, 255, 20, np.uint8(con*0.01*255)]
+                        self.im_array[int(self.x_scale_factor*(match.x- self.x_0))-i, int(self.y_scale_factor*(match.y-self.y_0))+j] = [57, 255, 20, np.uint8(con*0.01*255)]
 
 
     # need to ask Andy about this
