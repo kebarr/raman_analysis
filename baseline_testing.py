e3b4879d7e74e18a7595baa4a5f5dd4a58ce4fdf
kebarr <katarzinia@gmail.com>
2019-09-04 16:51:11 +0100

    made the baseline faster by stopping it from removing the baseline

--- baseline_testing.py
+++ baseline_testing.py
@@ -74,66 +74,78 @@ def wrapper(func, *args, **kwargs):
 wrapped = wrapper(test_baseline_new, data, baseline_als_original)
 timeit.timeit(wrapped, number=10) 
 
+# the following will be defined for each signal, when their initial values are the same:
+
+deriv_order=2
+# Compute the fixed derivative of identity (D).
+d = np.zeros(deriv_order*2 + 1, dtype=int)
+d[deriv_order] = 1
+d = np.diff(d, n=deriv_order)
+n = data.iloc[0].shape[0]-2
+k = len(d)
+s = float(1e6)
+
+# Here be dragons: essentially we're faking a big banded matrix D,
+# doing s * D.T.dot(D) with it, then taking the upper triangular bands.
+diag_sums = np.vstack([
+    np.pad(s*np.cumsum(d[-i:]*d[:i]), ((k-i,0),), 'constant')
+    for i in range(1, k+1)])
+upper_bands = np.tile(diag_sums[:,-1:], n)
+upper_bands[:,:k] = diag_sums
+for i,ds in enumerate(diag_sums):
+    upper_bands[i,-i-1:] = ds[::-1][:i+1]
+upper_bands = upper_bands
+smoothness_param=1e6
+
 
 class WhittakerSmoother(object):
-  def __init__(self, signal, smoothness_param, deriv_order=1):
+  def __init__(self, signal):
     self.y = signal
-    assert deriv_order > 0, 'deriv_order must be an int > 0'
-    # Compute the fixed derivative of identity (D).
-    d = np.zeros(deriv_order*2 + 1, dtype=int)
-    d[deriv_order] = 1
-    d = np.diff(d, n=deriv_order)
-    n = self.y.shape[0]
-    k = len(d)
-    s = float(smoothness_param)
-
-    # Here be dragons: essentially we're faking a big banded matrix D,
-    # doing s * D.T.dot(D) with it, then taking the upper triangular bands.
-    diag_sums = np.vstack([
-        np.pad(s*np.cumsum(d[-i:]*d[:i]), ((k-i,0),), 'constant')
-        for i in xrange(1, k+1)])
-    upper_bands = np.tile(diag_sums[:,-1:], n)
-    upper_bands[:,:k] = diag_sums
-    for i,ds in enumerate(diag_sums):
-      upper_bands[i,-i-1:] = ds[::-1][:i+1]
-    self.upper_bands = upper_bands
 
   def smooth(self, w):
-    foo = self.upper_bands.copy()
+    foo = upper_bands.copy()
     foo[-1] += w  # last row is the diagonal
     return solveh_banded(foo, w * self.y, overwrite_ab=True, overwrite_b=True)
 
 from scipy.linalg import solveh_banded
 #https://gist.github.com/perimosocordiae/efabc30c4b2c9afd8a83
-def als_baseline(intensities, asymmetry_param=0.05, smoothness_param=1e6,max_iters=5, conv_thresh=1e-5, verbose=False):
+def als_baseline(intensities, asymmetry_param=0.05, max_iters=5, conv_thresh=1e-5, verbose=False):
     '''Computes the asymmetric least squares baseline.
     * http://www.science.uva.nl/~hboelens/publications/draftpub/Eilers_2005.pdf
     smoothness_param: Relative importance of smoothness of the predicted response.
     asymmetry_param (p): if y > z, w = p, otherwise w = 1-p.
                         Setting p=1 is effectively a hinge loss.
     '''
-    smoother = WhittakerSmoother(intensities, smoothness_param, deriv_order=2)
+    smoother = WhittakerSmoother(intensities)
     # Rename p for concision.
     p = asymmetry_param
     # Initialize weights.
-    w = np.ones(intensities.shape[0])
-    for i in xrange(max_iters):
+    w_init = np.ones(data.iloc[0].shape[0]-2)
+    for i in range(max_iters):
         z = smoother.smooth(w)
         mask = intensities > z
         new_w = p*mask + (1-p)*(~mask)
         conv = np.linalg.norm(new_w - w)
-        if verbose:
-            print i+1, conv
         if conv < conv_thresh:
             break
         w = new_w
-    else:
-        print 'ALS did not converge in %d iterations' % max_iters
     return z
 
+data= pd.read_csv('uploads/raw_example.csv', sep='\t', encoding='utf-8')
+data = data.rename(columns={'Unnamed: 0' : 'x', 'Unnamed: 1' : 'y'})
 wrapped = wrapper(test_baseline_new, data, als_baseline)
-timeit.timeit(wrapped, number=10) # 413 seconds for 10 iteractions
+timeit.timeit(wrapped, number=10) # 36 seconds for original... so that's why i used this one
+# taking stuff out to front improves it- down to 26 seconds for 10 iterations. 10*999 = 10k, so for 70k its 3.5 minutes
+# moving w intialisation out makes it slower!!!
 
+
+
+wrapped = wrapper(WhittakerSmoother, data.iloc[0])
+timeit.timeit(wrapped, number=10) # initialisation is almost instantaneous
+ata= pd.read_csv('uploads/raw_example.csv', sep='\t', encoding='utf-8')
+data = data.rename(columns={'Unnamed: 0' : 'x', 'Unnamed: 1' : 'y'})
+
+res = test_baseline_new(data, als_baseline)
 from scipy.signal import savgol_filter
 
 def func(x, a, b, c, d, e, f, g):
@@ -157,7 +169,32 @@ def baseline_poly_smooth(data):
     
 
 wrapped = wrapper(test_baseline_new, data, baseline_poly)
-timeit.timeit(wrapped, number=10)
+timeit.timeit(wrapped, number=10) # 79 seconds for 10 iterations
 
 wrapped = wrapper(test_baseline_new, data, baseline_poly_smooth)
-timeit.timeit(wrapped, number=10)
+timeit.timeit(wrapped, number=10) # 94 seconds for 10 iterations
+
+# try without smoothing
+def als_baseline_no_smooth(intensities, asymmetry_param=0.05, max_iters=5, conv_thresh=1e-5, verbose=False):
+    '''Computes the asymmetric least squares baseline.
+    * http://www.science.uva.nl/~hboelens/publications/draftpub/Eilers_2005.pdf
+    smoothness_param: Relative importance of smoothness of the predicted response.
+    asymmetry_param (p): if y > z, w = p, otherwise w = 1-p.
+                        Setting p=1 is effectively a hinge loss.
+    '''    # Rename p for concision.
+    p = asymmetry_param
+    # Initialize weights.
+    w = np.ones(data.iloc[0].shape[0]-2)
+    for i in range(max_iters):
+        mask = intensities > w
+        new_w = p*mask + (1-p)*(~mask)
+        conv = np.linalg.norm(new_w - w)
+        if conv < conv_thresh:
+            break
+        w = new_w
+    return w
+
+data= pd.read_csv('uploads/raw_example.csv', sep='\t', encoding='utf-8')
+data = data.rename(columns={'Unnamed: 0' : 'x', 'Unnamed: 1' : 'y'})
+wrapped = wrapper(test_baseline_new, data, als_baseline_no_smooth)
+timeit.timeit(wrapped, number=10) # down to 15s
